12a8517f928d5fc64e9d869569ca255dd783bd78
FabrettiMarco <mfabretti2000@gmail.com>
2021-11-23 21:30:40 +0100

    corretto

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,5 +1,7 @@
 package org.generation.italy.geometria;
+
 import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,15 +9,15 @@ public class Main {
 		int altezza;
 		Scanner cIn = new Scanner(System.in);
 		System.out.println("Inserire la base ");
-		base=cIn.nextInt();
+		base = cIn.nextInt();
 		System.out.println("inserire l'altezza ");
-		altezza=cIn.nextInt();
-		
-		Rettangolo r = new Rettangolo(base, altezza); 
-		System.out.println("la base è: "+ r.base);
-		System.out.println("l'altezza è: "+ r.altezza);
-		System.out.println("il perimetro è: "+ r.perimetro(r.base, r.altezza));
-		System.out.println("l'area è: "+ r.area(r.base, r.altezza));
+		altezza = cIn.nextInt();
+
+		Rettangolo r = new Rettangolo(base, altezza);
+		System.out.println("la base è: " + r.base);
+		System.out.println("l'altezza è: " + r.altezza);
+		System.out.println("il perimetro è: " + r.perimetro());
+		System.out.println("l'area è: " + r.area());
 		cIn.close();
 	}
 
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -4,18 +4,18 @@ public class Rettangolo {
 	int base;
 	int altezza;
 	
-	public Rettangolo(int base, int altezza){
-		this.base=base;
-		this.altezza=altezza;
+	public Rettangolo(int base, int altezza) {
+		this.base = base;
+		this.altezza = altezza;
 	}
-	
-	public  int perimetro(int base, int altezza) {
-		int perimetro=((base*2)+(altezza*2));
+
+	public int perimetro() {
+		int perimetro = ((base * 2) + (altezza * 2));
 		return perimetro;
 	}
-	
-	public int area(int base, int altezza) {
-		int area=base*altezza;
+
+	public int area() {
+		int area = base * altezza;
 		return area;
 	}
 }
